39dba52997b9081e97d4bad97d6731678b06a844
danved20 <clarodany309h@gmail.com>
2023-10-07 16:59:13 -0400

    este es un comentario d para la migracion horarios

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReservaController;
-use App\Http\Controllers\ClienteController;
-use App\Http\Controllers\HorarioController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,20 +11,28 @@ use App\Http\Controllers\HorarioController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Auth::routes();
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('/reservas',ReservaController::class);
-Route::resource('/clientes',ClienteController::class);
-Route::resource('/horarios',HorarioController::class);
- Route::get('/reporte-reservas', 'ReporteController@generarReporte'); 
+require __DIR__.'/auth.php';
