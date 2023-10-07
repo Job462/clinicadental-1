f937441094e9e5a2ea5ed793c0f7f1bfac2710fc
bel-dravon <139442144+bel-dravonr@users.noreply.github.com>
2023-10-07 16:56:53 -0400

    Se creo la migracion en la base de datosd el a tabla clientes

--- database/migrations/2023_09_18_214606_create_clientes_table.php
+++ database/migrations/2023_09_18_214606_create_clientes_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        //Se creo la migracion de clientes en la base de datos
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
             $table->string('nombre',120);
